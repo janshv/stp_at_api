542d4ade34952def36e33da499a23db1a58f2000
janshv <janshv.dmitry@mail.ru>
2026-05-27 15:07:08 +0300

    Знакомство с API клиентами; практика: Написание API клиента

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from httpx import Client, URL, Response, QueryParams
+
+from clients.users.public_users_client import PublicUsersClient
+from tools.fakers import get_random_email, get_random_lastname, get_random_firstname
+
 print('Xola!')
+
+usr = {
+  "email": get_random_email(),
+  "password": "12345678",
+  "lastName": get_random_lastname(),
+  "firstName": get_random_firstname(),
+  "middleName": get_random_firstname()
+}
+
+puc = PublicUsersClient(Client(base_url="http://localhost:8000"))
+
+dt = puc.create_user_api(request=usr).json()
+
+print(dt)
