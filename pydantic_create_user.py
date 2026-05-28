ee07c0c79c0b3f5b5a5d27636f3041d1ebb9bdc3
janshv <janshv.dmitry@mail.ru>
2026-05-28 19:23:09 +0300

    Знакомство с Pydantic; практика написания Pydantic-моделей

---
+++ pydantic_create_user.py
@@ -0,0 +1,29 @@
+import uuid
+
+from pydantic import BaseModel, Field, EmailStr, HttpUrl, ValidationError
+
+
+class UserSchema(BaseModel):
+    """ Модель данных пользователя """
+    id: str
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+
+class CreateUserRequestSchema(BaseModel):
+    """Модель запроса на создание пользователя """
+    email: EmailStr
+    password: str
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+class CreateUserResponseSchema(BaseModel):
+    """ Модель ответа с данными созданного пользователя"""
+    id: str
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
