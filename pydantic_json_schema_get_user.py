6063012472ef04e2ff4b0eb3955004aaa7656498
janshv <janshv.dmitry@mail.ru>
2026-05-29 15:06:38 +0300

    Валидация  schema; практикуемся работы с  schema

---
+++ pydantic_json_schema_get_user.py
@@ -0,0 +1,35 @@
+from clients.private_http_builder import AuthenticationUserSchema
+from clients.users.private_users_client import get_private_users_client
+from clients.users.public_users_client import get_public_users_client
+from clients.users.users_schema import CreateUserRequestSchema, GetUserResponseSchema
+from tools.assertions.schema import validate_json_schema
+from tools.fakers import get_random_email, get_random_lastname, get_random_firstname
+
+public_users_client = get_public_users_client()
+
+create_user_request = CreateUserRequestSchema(
+    email=get_random_email(),
+    password="string",
+    lastName=get_random_lastname(),
+    firstName=get_random_firstname(),
+    middleName=get_random_firstname()
+)
+
+create_user_response = public_users_client.create_user(create_user_request)
+print('Create user data:', create_user_response)
+
+authentication_user = AuthenticationUserSchema(
+    email=create_user_request.email,
+    password=create_user_request.password
+)
+private_users_client = get_private_users_client(authentication_user)
+
+get_user_response = private_users_client.get_user_api(create_user_response.user.id)
+
+get_user_response_schema = GetUserResponseSchema.model_json_schema()
+print('User schema:', get_user_response_schema)
+validate_json_schema(instance=get_user_response.json(), schema=get_user_response_schema)
+
+
+
+
