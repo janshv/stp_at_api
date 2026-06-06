5024a4e81baf554cfd0f8fa85fdcc3a4a0c462ae
janshv <janshv.dmitry@mail.ru>
2026-06-06 13:47:52 +0300

    Added run autotests on CI

--- config.py
+++ config.py
@@ -26,10 +26,6 @@ class Settings(BaseSettings):
 
     test_data: TestDataConfig
     http_client: HTTPClientConfig
-
-    # test_data: Optional[TestDataConfig] = None
-    # http_client: Optional[HTTPClientConfig] = None
-
     allure_results_dir: DirectoryPath
 
     @classmethod
@@ -37,18 +33,8 @@ class Settings(BaseSettings):
         allure_results_dir = DirectoryPath("./allure-results")  # Создаем объект пути к папке
         allure_results_dir.mkdir(exist_ok=True)  # Создаем папку allure-results, если она не существует
 
-        # test_cfg = TestDataConfig(image_png_file=FilePath("./testdata/files/image.png"))
-        # http_cfg = HTTPClientConfig(url=HttpUrl("http://localhost:8000"), timeout=100.0)
-
-
         # Передаем allure_results_dir в инициализацию настроек
         return Settings(allure_results_dir=allure_results_dir)
-
-        # return Settings(
-        #     test_data=test_cfg,
-        #     http_client=http_cfg,
-        #     allure_results_dir=allure_results_dir
-        # )
 
 
 settings = Settings.initialize()
